59a6b758e2bb68571f025f41c7e70fcdb17fe84e
Damien Lachaume <44409901+damienlachaume@users.noreply.github.com>
2025-03-02 12:19:54 +0100

    feat: add `anyhow` error handling support

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,9 @@
 mod domain;
 
 pub use domain::Address;
+
+/// Alias for [anyhow::Error]
+pub type AnyhowError = anyhow::Error;
+
+/// Alias for [anyhow::Result]
+pub type AnyhowResult<T> = anyhow::Result<T, AnyhowError>;
